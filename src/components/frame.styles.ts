cd5d7e60b743f933d687273e3825fcf3b3b2163f
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-09-24 11:22:14 +0100

    Adjust frame styles

--- src/components/frame.styles.ts
+++ src/components/frame.styles.ts
@@ -3,10 +3,10 @@ import styled from "@emotion/styled"
 export const StyledFrame = styled.div`
   position: relative;
   aspect-ratio: 1;
+  width: 400px;
   height: 400px;
   max-width: min(90vw, 90vh);
   max-height: min(90vw, 90vh);
   border: 1px solid darkblue;
   cursor: pointer;
-  display: flex;
 `
