40f2f294e50594b1ada8b39f331cc8234b6c7aa9
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-10-01 21:58:18 +0100

    Remove sample puzzle and some utils

--- src/logic/sudoku-puzzle-utils.ts
+++ src/logic/sudoku-puzzle-utils.ts
@@ -1,16 +1,4 @@
-import { Digit, Digits } from "./types"
-
-const charToDigit = (ch: string): Digit => {
-  const n = Number(ch)
-  if (Number.isInteger(n) && n >= 1 && n <= 9) return n as Digit
-  return 0
-}
-
-const stringToDigitArray = (s: string): Digit[] =>
-  Array.from(s).map(charToDigit)
-
-export const stringsToDigits = (ss: string[]): Digits =>
-  ss.flatMap(stringToDigitArray) as Digits
+import { Digits } from "./types"
 
 export const getInitialValueIndices = (digits: Digits) =>
   digits.flatMap((digit, index) => digit === 0 ? [] : [index])
