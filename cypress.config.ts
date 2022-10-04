a8865df118c665b56d8f4a910d804c40b59d6a9a
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-10-04 10:17:25 +0100

    Add e2e test re scanning and solving a Sudoku puzzle

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,18 @@
-import { defineConfig } from "cypress";
+/// <reference types="cypress" />
+
+import { defineConfig } from "cypress"
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+    baseUrl: "http://localhost:3333/sudoku-buster-react",
+    setupNodeEvents(on: Cypress.PluginEvents, _config: Cypress.PluginConfigOptions): void {
+      on("before:browser:launch", (
+        _browser: Cypress.Browser,
+        launchOptions: Cypress.BrowserLaunchOptions
+      ) => {
+        launchOptions.args.push("--use-file-for-fake-video-capture=cypress/fixtures/scanning.y4m")
+        return launchOptions
+      })
+    }
+  }
+})
--- cypress/e2e/tests.cy.ts
+++ cypress/e2e/tests.cy.ts
@@ -2,18 +2,39 @@
 
 /// <reference types="cypress" />
 
-describe("tests", () => {
-  it("test 1", () => {
-    cy.visit("http://localhost:3333/sudoku-buster-react")
-    cy.findByText("Tap the big square to scan a puzzle")
+const INITIAL_MESSAGE = "Tap the big square to scan a puzzle"
+const SCANNING_MESSAGE = "Tap the big square to cancel"
+const SCANNED_MESSAGE = "Tap the big square to start over"
+
+describe("e2e tests", () => {
+  it("should display the correct message when in Initial mode", () => {
+    cy.visit("/")
+    cy.findByText(INITIAL_MESSAGE)
   })
 
-  it("test 2", () => {
-    cy.visit("http://localhost:3333/sudoku-buster-react")
-    cy.findByText("Tap the big square to scan a puzzle")
+  it("should display the correct messages when changing mode from Initial => Scanning => Initial", () => {
+    cy.visit("/")
+
+    cy.findByText(INITIAL_MESSAGE)
+
     cy.findByTestId("frame").click()
-    cy.findByText("Tap the big square to cancel")
+    cy.findByText(SCANNING_MESSAGE)
+
     cy.findByTestId("frame").click()
-    cy.findByText("Tap the big square to scan a puzzle")
+    cy.findByText(INITIAL_MESSAGE)
+  })
+
+  it("should display the correct messages when changing mode from Initial => Scanning => Scanned => Initial", () => {
+    cy.visit("/")
+
+    cy.findByText(INITIAL_MESSAGE)
+
+    cy.findByTestId("frame").click()
+    cy.findByText(SCANNING_MESSAGE)
+
+    cy.findByText(SCANNED_MESSAGE, { timeout: 10000 })
+
+    cy.findByTestId("frame").click()
+    cy.findByText(INITIAL_MESSAGE)
   })
 })
